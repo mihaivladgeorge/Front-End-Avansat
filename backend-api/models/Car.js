103ce96a324e22755057dbf82325578b0ca5ef6c
Vlad Mihai <vladgeorgemihai2001@gmail.com>
2023-01-24 17:51:17 +0200

    Added models for car, cart, order, user

---
+++ backend-api/models/Car.js
@@ -0,0 +1,15 @@
+const mongoose = requier("mongoose")
+
+const CarSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true, unique: true },
+        description: { type: String, required: true },
+        image: { type: String, required: true },
+        category: { type: Array },
+        dimensions: { type: String },
+        price: { type: String, required: true },
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("Car", CarSchema);
