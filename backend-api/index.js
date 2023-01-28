ffb24ca2c9a553c7d1b7668521630bffe58a264b
Vlad Mihai <vladgeorgemihai2001@gmail.com>
2023-01-28 22:20:15 +0200

    Addded authentication

--- backend-api/index.js
+++ backend-api/index.js
@@ -2,6 +2,7 @@ const express   = require("express");
 const mongoose  = require("mongoose");
 const dotenv    = require("dotenv");
 const userRoute = require("./routes/user")
+const authRoute = require("./routes/authentication")
 
 const app = express();
 dotenv.config();
@@ -16,6 +17,7 @@ mongoose.connect(
 
 app.use(express.json());
 app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(3000, ()=>{
     console.log("Node backend server running...");
